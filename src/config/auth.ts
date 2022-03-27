8e547360a76fae22561ca29e71f8158125c605f8
Leonardo Soares <leonardoxp9d@gmail.com>
2022-03-27 13:09:55 -0300

    refactoring user authentication

---
+++ src/config/auth.ts
@@ -0,0 +1,11 @@
+/* configurações da autenticação */
+
+/* export default - para pegar mais facil as variaveis 
+expires_refresh_token_days - quantos dias que vai expirar o refresh token */
+export default {
+  secret_token: "629154f7128dbc61abbdf5264038e57d",
+  expires_in_token: "15m",
+  secret_refresh_token: "a61f228c4600cab3fda6b7f22cb598e9",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
