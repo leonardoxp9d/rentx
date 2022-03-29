cde0a0cdca24d4491379e74a546bddf3a824de44
Leonardo Soares <leonardoxp9d@gmail.com>
2022-03-29 17:44:59 -0300

    test coverage report

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -2,26 +2,24 @@ import { hash } from "bcrypt";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import request from "supertest";
 import { Connection } from "typeorm";
-import { v4 as uuidV4 } from "uuid";
+import { v4 as uuid } from "uuid";
 
 import { app } from "@shared/infra/http/app";
-import createConection from "@shared/infra/typeorm";
+import createConnection from "@shared/infra/typeorm";
 
 let connection: Connection;
-
-describe("List Categories", () => {
+describe("Create Category Controller", () => {
   beforeAll(async () => {
-    connection = await createConection();
-
+    connection = await createConnection();
     await connection.runMigrations();
 
-    const id = uuidV4();
+    const id = uuid();
     const password = await hash("admin", 8);
 
     await connection.query(
       `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license ) 
-      values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
-    `
+        values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'XXXXXX')
+      `
     );
   });
 
@@ -30,7 +28,7 @@ describe("List Categories", () => {
     await connection.close();
   });
 
-  it("Should be able to list all categories", async () => {
+  it("should be able to list all categories ", async () => {
     const responseToken = await request(app).post("/sessions").send({
       email: "admin@rentx.com.br",
       password: "admin",
